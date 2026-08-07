a98b267114a16689185d24197b602c10be3c1db2
vishesh4u <visheshgurjar692007@gmail.com>
2026-08-07 22:19:35 +0530

    js till now
    Add mouseenter event listener to nav for animation.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var nav = document.querySelector("nav")
 
+nav.addEventListener("mouseenter",function(){
+
+    var tl = gsap.to("#nav-bottom",{
+        height:"25vh",
+    });
+    tl.to("nav h5",{
+        display:"initial"
+    })
+});
+
+// nav.addEventListener("mouseleave", function () {
+//     gsap.to("#nav-bottom", {
+//         height: 0,
+//         duration: 0.5,
+//         ease: "power2.out"
+//     });
+// });
