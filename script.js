0216179b968b67ff089bf221a7eef0c43447cbfa
vishesh4u <visheshgurjar692007@gmail.com>
2026-08-10 21:06:41 +0530

    Refactor nav animation and add right element effects

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-var nav = document.querySelector("nav")
+function navAnimation(){
+    var nav = document.querySelector("nav")
 
 nav.addEventListener("mouseenter",function(){
+    var tl = gsap.timeline()
 
-    var tl = gsap.to("#nav-bottom",{
-        height:"25vh",
+    tl.to("#nav-bottom",{
+        height:"21vh"
     });
-    tl.to("nav h5",{
-        display:"initial"
+    tl.to(".nav-elem h5",{
+        display:"block"
+    })
+    tl.from(".nav-part2 h5 span",{
+        y:0,
+        // duration:0.5,
+        stagger:{
+            amount:0.6
+        }
     })
 });
+nav.addEventListener("mouseleave",function(){
+    let tl = gsap.timeline()
+    tl.to(".nav-part2 h5 span",{
+        y:25,
+        // duration:0.5,
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to(".nav-part2 h5",{
+        display:"none",
+        duration:0.1
+      })  
+        tl.to("#nav-bottom",{
+            height:0,
+            duration:0.2
+        })
+});
+}
 
+navAnimation()
+
+//for learning
 // nav.addEventListener("mouseleave", function () {
 //     gsap.to("#nav-bottom", {
 //         height: 0,
@@ -17,4 +48,23 @@ nav.addEventListener("mouseenter",function(){
 //         ease: "power2.out"
 //     });
 // });
-// Work in progress because I am lazy today 
+
+var rightElems = document.querySelectorAll(".right-elem")
+
+rightElems.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+       gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
+    })
+    elem.addEventListener("mouseleave", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        })
+    })
+    elem.addEventListener("mousemove",function(){
+        console.log(elem.getBoundingClientRect().y)
+    })
+})
